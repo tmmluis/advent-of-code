2db19d5d2ab1e543bb61f27e4829a7a608220c74
tmmluis <tmmluis@gmail.com>
2020-07-31 12:44:05 +0200

    Day 1 solution

---
+++ day1.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+fs.readFile('day1.txt' , (err, data) => {
+    if (err) {
+        throw err
+    }
+    
+    const modulesMass = data.toString().split('\n').map(value => Number(value))
+    
+    const part1Answer = modulesMass.reduce((accumulator, currentValue) => accumulator + computeFuel(currentValue), 0)
+    console.log(part1Answer)
+
+    const part2Answer = modulesMass.reduce((accumulator, currentValue) => accumulator + computeTotalFuel(currentValue), 0)
+    console.log(part2Answer)
+})
+
+function computeFuel(mass) {
+    return Math.floor(mass / 3) - 2
+}
+
+function computeTotalFuel(mass) {
+    let fuel = Math.floor(mass / 3) - 2
+
+    if (fuel <= 0) {
+        return 0
+    }
+
+    return fuel + computeTotalFuel(fuel)
+}
